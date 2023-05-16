4dc135ad38a6f71740190033378f650a092c922d
Aaron Gibson <9agibson@gmail.com>
2023-05-16 08:24:07 -0500

    reducer

--- client/src/context/AppContext.js
+++ client/src/context/AppContext.js
@@ -1,6 +1,6 @@
 import React, { useReducer, useContext, useEffect } from 'react';
 
-import reducer from './reducer';
+import reducer from '../../src/context/reducer';
 import axios from 'axios';
 import {
   DISPLAY_ALERT,
