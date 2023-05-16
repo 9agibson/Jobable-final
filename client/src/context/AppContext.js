1cdd0a2666d7bcee136e5935857caed559291444
Aaron Gibson <9agibson@gmail.com>
2023-05-16 11:05:42 -0500

    reducer issue

--- client/src/context/AppContext.js
+++ client/src/context/AppContext.js
@@ -1,6 +1,6 @@
 import React, { useReducer, useContext, useEffect } from 'react';
 
-import reducer from './reducer'
+import reducer from '../../src/context/reducer.js'
 import axios from 'axios';
 import {
   DISPLAY_ALERT,
